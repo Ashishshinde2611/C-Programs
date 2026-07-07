91a3594e0181370cc5e4dece5924a5809c3177d5
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 16:08:28 +0530

    Sync Pointers/Pointer-Exercise 3/VowelsString/vowelsCount.c

---
+++ Pointers/Pointer-Exercise 3/VowelsString/vowelsCount.c
@@ -0,0 +1,37 @@
+/*
+
+  write a  Program to count vowels and consonants in a string using pointer.
+
+*/
+
+
+#include <stdio.h>  
+int main()
+{
+	char str[100], *ptr;
+    int vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]s", str);
+
+    ptr = str;
+
+    while(*ptr != '\0') {
+        char ch = *ptr;
+        if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+               ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+        ptr++;
+    }
+
+    printf("Number of vowels: %d\n", vowels);
+    printf("Number of consonants: %d\n", consonants);
+    
+	
+	return 0;
+}
