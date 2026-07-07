c4b34151e89120ab86d03128d47f7c9652b52a20
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 15:57:51 +0530

    Sync if-else/if-else_Exercise1/EvenOdd/EvenOdd.c

---
+++ if-else/if-else_Exercise1/EvenOdd/EvenOdd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int no1; 
+	printf("Enther the number");
+	scanf("%d",&no1);
+	if(no1%2==0){
+		printf("Number is Even");
+}else{
+		printf("Nuber is Odd");
+}
+return 0 ;
+}
