f3300c937da393db95ec460d67daba83a1967449
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 15:38:33 +0530

    Sync 2DArray/2DArrayExercise3/patten/Patten.c

---
+++ 2DArray/2DArrayExercise3/patten/Patten.c
@@ -0,0 +1,38 @@
+//Accept  4*4 matrix and display the following patern
+
+/*
+
+  1 1 1 *
+  1 1 * 0
+  1 * 0 0
+  * 0 0 0  
+  
+*/
+#include <stdio.h>
+
+int main() {
+    int a[4][4];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (j < 4 - i - 1)
+                a[i][j] = 1;
+            else if (j == 4 - i - 1)
+                a[i][j] = -1; 
+            else
+                a[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (a[i][j] == -1)
+                printf("* ");
+            else
+                printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
