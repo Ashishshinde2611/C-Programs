10668324c0b3cfb3603a696984f2a88b99b0be4d
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 15:36:16 +0530

    Sync 2DArray/2DArrayExercise1/Display Elements/LeftDiagonal.c

---
+++ 2DArray/2DArrayExercise1/Display Elements/LeftDiagonal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    printf("Enter number of rows (n): ");
+    scanf("%d", &n);
+    printf("Enter number of columns (m): ");
+    scanf("%d", &m);
+
+    int arr[n][m];
+	
+	printf(" matrix (%d x %d):\n", n, m);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &arr[i][j]);
+     
+        }
+    }
+	printf("left Diagonal\n");
+    for (int i = 0; i < n; i++) {
+            printf(" %d",arr[i][i]); 
+ 
+    }
+	 
+	return 0 ;
+}
