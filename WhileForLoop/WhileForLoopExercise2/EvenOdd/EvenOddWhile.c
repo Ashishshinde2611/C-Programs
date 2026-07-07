f672dd16fef63e55d7702cb88e991bc993654918
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 16:20:36 +0530

    Sync WhileForLoop/WhileForLoopExercise2/EvenOdd/EvenOddWhile.c

---
+++ WhileForLoop/WhileForLoopExercise2/EvenOdd/EvenOddWhile.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i = 1;
+    int sumEven = 0, sumOdd = 0;
+	int diffEven = 0, diffOdd = 0;
+    int multiEven = 1 , mutiOdd = 1;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+	
+       
+
+    while (i <= n)
+    {
+        if (i % 2 == 0){
+            sumEven += i;
+		    if (diffEven == 0)     
+                diffEven = i;
+            else
+                diffEven -= i;
+			multiEven *= i;
+		}
+        else{
+            sumOdd += i;
+             if (diffOdd == 0)     
+                diffOdd = i;
+            else
+                diffOdd -= i;
+			mutiOdd *= i;
+		}
+			
+        i++;
+    }
+
+    printf("Sum of Even Numbers = %d\n", sumEven);
+    printf("Sum of Odd Numbers  = %d\n", sumOdd);
+	printf("Difference of Even Numbers = %d\n", diffEven);
+    printf("Difference of Odd Numbers  = %d\n", diffOdd);
+	printf("multiplication of Even Numbers = %d\n", multiEven);
+    printf("multiplication of Odd Numbers  = %d\n", mutiOdd);
+
+    return 0;
+}
