f9a67562967382100a0917c88f3bb53e898f59a7
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 15:36:04 +0530

    Sync 2DArray/2DArrayExercise1/Display Elements/4x3Matrix.c

---
+++ 2DArray/2DArrayExercise1/Display Elements/4x3Matrix.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int arr[4][3];
+    printf("Enter the array elements:\n");
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("Array elements are:\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
