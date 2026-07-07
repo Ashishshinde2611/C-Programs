d17d0fcd2c86de251b06771173d08e3cdc06d345
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 15:41:35 +0530

    Sync ArithmeticOperations/Arithmetic Operation-2/Swaping/Swaping.c

---
+++ ArithmeticOperations/Arithmetic Operation-2/Swaping/Swaping.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int no1, no2, temp;
+
+    printf("Enter first number (no1): ");
+    scanf("%d", &no1);
+	
+
+    printf("Enter second number (no2): ");
+    scanf("%d", &no2);
+	
+
+    printf("\nBefore swapping: no1 = %d, no2 = %d\n", no1, no2);
+
+    temp = no1;
+    no1 = no2;
+    no2 = temp;
+
+    printf("After swapping: no1 = %d, no2 = %d\n", no1, no2);
+
+    return 0;
+}
