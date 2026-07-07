4b2b9178ccbbce0df0d1b2230097c161cd0f4d37
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 15:58:28 +0530

    Sync if-else/if-else_Exercise1/PostiveNegative/PosNegative.c

---
+++ if-else/if-else_Exercise1/PostiveNegative/PosNegative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{ 
+int a;
+printf("Enter the number :");
+scanf("%d",&a);
+if (a>0){
+	printf("Number is positive");
+}else{
+	printf("Number is negative");
+}
+return 0 ;
+}
