36cad82e74327a89b7d9786d11e0133f3a259413
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 15:53:15 +0530

    Sync Enum/Enum_Exercise1/Question4.c

---
+++ Enum/Enum_Exercise1/Question4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+enum Month {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+int main() {
+    printf("Numeric value of March = %d\n", March);
+    return 0;
+}
