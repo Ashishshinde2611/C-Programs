7c23a657100ce93ba99cc57a2825aa980ae8d626
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 16:21:54 +0530

    Sync WhileForLoop/WhileForLoopExercise2/Table/TableWhile.c

---
+++ WhileForLoop/WhileForLoopExercise2/Table/TableWhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Multiplication Table of %d:\n", n);
+
+    while (i <= 10)  
+    {
+        printf("%d x %d = %d\n", n, i, n * i);
+        i++;
+    }
+
+    return 0;
+}
