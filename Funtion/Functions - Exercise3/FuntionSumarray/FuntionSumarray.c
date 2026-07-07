7277dd691d2f374c2c19973d032e436e78854e7f
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 15:57:00 +0530

    Sync Funtion/Functions - Exercise3/FuntionSumarray/FuntionSumarray.c

---
+++ Funtion/Functions - Exercise3/FuntionSumarray/FuntionSumarray.c
@@ -0,0 +1,38 @@
+//Write a function to calculate the sum of all elements of an array
+
+
+
+#include <stdio.h>
+
+
+int sumArray(int arr[], int n);
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+   
+    int total = sumArray(arr, n);
+
+    printf("Sum of all elements = %d\n", total);
+
+    return 0;
+}
+
+
+int sumArray(int arr[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
