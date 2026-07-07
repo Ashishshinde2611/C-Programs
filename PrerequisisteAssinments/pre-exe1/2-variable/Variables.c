950566fb0e1459b74b75050026796de4ff5c9d75
Ashish Shinde <ashishshinde2611@gmail.com>
2026-07-07 16:10:34 +0530

    Sync PrerequisisteAssinments/pre-exe1/2-variable/Variables.c

---
+++ PrerequisisteAssinments/pre-exe1/2-variable/Variables.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(void)
+{
+	int i = 10;
+	float f =10.5f;
+	double d = 5.05246;
+	char c ='A';
+	
+	printf("\n\n");
+	printf("i=%d\n",i);
+	printf("f=%f\n",f);
+	printf("d=%lf\n",d);
+	printf("c=%c\n",c);
+	printf("\n\n");
+	
+	i=56;
+	f=9.45f;
+	d=75.1793;
+	c='b';
+	
+	printf("i=%d\n",i);
+	printf("f=%f\n",f);
+	printf("d=%lf\n",d);
+	printf("c=%c\n",c);
+	printf("\n\n");
+	
+	return(0);
+}	
+	
